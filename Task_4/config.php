e11ab5afb7dc35e988a8c6fc7494382a4d34e3e4
RomanAngelovskij <turbocom-mlt@yandex.ru>
2015-04-01 10:18:20 +0300

    Update config.php

--- Task_4/config.php
+++ Task_4/config.php
@@ -3,7 +3,7 @@ define ('SALT', '213sasd78');
 
 define ('DB_DRIVER', 'mysql');
 define ('DB_HOST', 'localhost');
-define ('DB_NAME', 'tests');
-define ('DB_USER', 'tests');
-define ('DB_PASSWORD', 'TestTask');
+define ('DB_NAME', '');
+define ('DB_USER', '');
+define ('DB_PASSWORD', '');
 ?>
